59bd164a7c31321688a159aa973c16ccdc18f5dd
NguyenHaPhuong <39210018+NguyenHaPhuong@users.noreply.github.com>
2018-06-05 22:38:39 +0800

    Delete RP_Challenge4.py

--- RP_Challenge4.py
+++
@@ -1,8 +0,0 @@
-# Write the code to find the MRT codes that starts with NS or EW or CC and has 1 or 2 digit(s). 
-
-import re
-listStrMRTCodes = ['NS15','EW4', 'NS234', 'CC8', 'D101', 'XY27', 'C400', 'EW32']
-for i in listStrMRTCodes:
-	result = re.search("^(NS|EW|CC)\d{1,2}$", i)
-	if result != None:
-		print (i)
